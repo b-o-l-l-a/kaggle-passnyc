02830bfa354d55eca10c232c1f3698ffd7d784d3
Gregory Bolla <greg.bolla@Gregorys-MacBook-Pro.local>
2018-08-06 08:09:28 -0600

    added enrollment to response df

--- src/models/model_utils.py
+++ src/models/model_utils.py
@@ -1,4 +1,4 @@
-# from sklearn import metrics
+from sklearn import metrics
 
 def adj_r2_score(lm, y, y_pred):
     adj_r2 = 1 - float(len(y)-1)/(len(y)-len(lm.coef_)-1)*(1 - metrics.r2_score(y,y_pred))
@@ -10,11 +10,13 @@ def get_pred_and_response_dfs(df):
     "SED Code", "Latitude", "Longitude", "Zip"]
     response_vars = ["num_testtakers", "num_offered", "pct_8th_graders_offered", "perc_testtakers", "perc_testtakers_quartile"]
     response_var = "perc_testtakers"
+    invalid_preds.extend(response_vars)
     
     # incl dbn and school_name for convenience and to id each row
-    response_df_cols = ["dbn", "school_name", response_var]
+    # incl enrollment to calculate addtl est'd num students
+    response_df_cols = ["dbn", "school_name", "grade_8_2017_enrollment", response_var]
     
     response_df = df[response_df_cols]
     pred_df = df.drop(response_df_cols, axis=1)
     
-    return pred_df, response_df, invalid_preds
+    return pred_df, response_df, invalid_preds, response_var
